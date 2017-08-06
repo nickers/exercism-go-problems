7f8f91374cf163ff77da1b3d0da63d4da8bf2693
Tomasz Wsuł <2nickers@gmail.com>
2017-08-06 11:52:40 +0200

    'bob' solution using regexp

--- bob/bob.go
+++ bob/bob.go
@@ -1,3 +1,32 @@
 package bob
 
+import "regexp"
+
 const testVersion = 3
+
+var emptyWords, maybeShouting, notShouting, isQuestion *regexp.Regexp
+
+// Hey says given text to Bob.
+func Hey(words string) string {
+
+	if emptyWords == nil {
+		emptyWords, _ = regexp.Compile("^\\s*$")
+		maybeShouting, _ = regexp.Compile("[A-Z]")
+		notShouting, _ = regexp.Compile("[a-z]")
+		isQuestion, _ = regexp.Compile("\\?\\s*$")
+	}
+
+	if emptyWords.MatchString(words) {
+		return "Fine. Be that way!"
+	}
+
+	if maybeShouting.MatchString(words) && !notShouting.MatchString(words) {
+		return "Whoa, chill out!"
+	}
+
+	if isQuestion.MatchString(words) {
+		return "Sure."
+	}
+
+	return "Whatever."
+}
