f9ff3d16c943d6ccdab86f27cedea2f1a2150b6f
Tomasz Wsuł <2nickers@gmail.com>
2017-08-06 13:32:15 +0200

    'error-handling' solved

---
+++ error-handling/error-handling.go
@@ -0,0 +1,31 @@
+package erratum
+
+const testVersion = 2
+
+func Use(o ResourceOpener, input string) (ret error) {
+	r, e := o()
+	for ; e != nil; r, e = o() {
+		switch e.(type) {
+		default:
+			return e
+		case TransientError:
+		}
+	}
+
+	defer func() {
+		r.Close()
+	}()
+
+	defer func() {
+		if x := recover(); x != nil {
+			if frobErr, ok := x.(FrobError); ok {
+				r.Defrob(frobErr.defrobTag)
+			}
+			ret, _ = x.(error)
+		}
+	}()
+
+	r.Frob(input)
+
+	return nil
+}
